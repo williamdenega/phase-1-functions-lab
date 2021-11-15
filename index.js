645369f3a2519183edd41ed9f0cca2b474e1d9bb
WilliamDenega <williamdenega@gmail.com>
2021-11-15 16:54:58 -0500

    commit tree 65db16f7fee029d393ecc8147eee74aa9144ac4f

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+
+//const headQuaters = 42;
+function distanceFromHqInBlocks(blocks){
+    return Math.abs(blocks- 42);
+}
+
+function distanceFromHqInFeet(blocks){
+    return Math.abs(distanceFromHqInBlocks(blocks)*264);
+}
+
+
+function distanceTravelledInFeet(start,destination){
+    return 264*Math.abs(destination-start);
+
+
+}
+
+
+function calculatesFarePrice(start,destination){
+    const distance = distanceTravelledInFeet(start,destination);
+    if (distance <= 400){
+        return 0;
+    }
+    else if (distance <= 2000){
+        return  .02 * (distance - 400);
+    }
+    else if (distance < 2500){
+        return 25;
+    }
+    else{
+      return 'cannot travel that far';
+    }
+
+}
